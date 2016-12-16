fd859b2137f6fe10a0a9ada4b4bce785c6d243aa
yama-3 <kazuhiro.yamabe@gmail.com>
2016-12-16 17:02:21 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+﻿
+# -*- coding: utf-8 -*-
+
+
+from selenium import webdriver
+
+
+if __name__ == '__main__':
+    driver = webdriver.PhantomJS()
+    url = 'http://cs.crosswarp.com/wf/hoge/test/DomPage'
+    driver.get(url)
+    data = driver.page_source.encode('utf-8')
+    print(data)
+    driver.save_screenshot('cw.png')
+    driver.quit()
+
